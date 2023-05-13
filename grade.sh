4d3fb059bfd08aa723d877c448a2c62d95ae3073
tengfongLee <tel003@ucsd.edu>
2023-05-12 17:55:48 -0700

    tester redirection
    just put the tester in the list-examples-grader, then the program will automatically move the file in to the current working directory, which is grading area in this case.

--- grade.sh
+++ grade.sh
@@ -14,13 +14,14 @@ fi
 git clone $1 student-submission
 echo 'Finished cloning'
 cp -r student-submission/ grading-area/
+cp ListTester.java/ grading-area/
 cd grading-area
 
 
 javac -cp ../lib/junit-4.13.2.jar:../libs/hamcrest-2.2.jar:. ../ListTester.java
 echo $?
 
-java -cp ../lib/junit-4.13.2.jar:../libs/hamcrest-2.2.jar:. org.junit.runner.JUnitCore ../ListTester > output.txt
+java -cp ../lib/junit-4.13.2.jar:../libs/hamcrest-2.2.jar:. org.junit.runner.JUnitCore ListTester > output.txt
 cat output.txt
 echo $?
 
